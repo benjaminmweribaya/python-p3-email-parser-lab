8cc32a87fbb777b2ed26e51dc32121e34e578b93
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-12-06 01:56:14 +0300

    Fix parsing to filter valid email addresses

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Use regex to extract valid email addresses
+        emails = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', self.email_addresses)
+        # Remove duplicates by converting to a set, then sort alphabetically
+        unique_emails = sorted(set(emails))
+        return unique_emails
